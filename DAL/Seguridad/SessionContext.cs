0740cd737bf5708c96c0f99d0b5b793d1667a66d
Kury <andres_kuryga@hotmail.com>
2025-10-18 18:05:06 -0300

    se piden las patentes del usuario que se logueo

--- DAL/Seguridad/SessionContext.cs
+++ DAL/Seguridad/SessionContext.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DAL.Seguridad
 {
@@ -11,11 +13,43 @@ namespace DAL.Seguridad
         public string UsuarioEmail { get; set; }
         public string NombreCompleto { get; set; }
 
+        public HashSet<string> Patentes { get; private set; }
+
         private SessionContext()
+        {
+            this.ClearSession();
+        }
+
+        public void ClearSession()
         {
             UsuarioId = null;
             UsuarioEmail = "Usuario deslogeado";
             NombreCompleto = "Usuario deslogeado";
+            Patentes = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public void SetPatentes(IEnumerable<BE.Patente> patentes)
+        {
+            Patentes = patentes == null
+                ? new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                : new HashSet<string>(
+                    patentes
+                        .Select(p => p?.NombrePatente ?? string.Empty)
+                        .Where(s => !string.IsNullOrWhiteSpace(s)),
+                    StringComparer.OrdinalIgnoreCase
+                  );
+        }
+
+        public bool TienePatente(string codigo)
+        {
+            if (string.IsNullOrWhiteSpace(codigo)) return false;
+            return Patentes.Contains(codigo);
+        }
+
+        public bool TieneAlguna(params string[] codigos)
+        {
+            if (codigos == null || codigos.Length == 0) return false;
+            return codigos.Any(c => TienePatente(c));
         }
     }
 }

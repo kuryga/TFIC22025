7376ce22d60aad9762a8555259ccd931b14bb3f7
Kury <andres_kuryga@hotmail.com>
2025-10-25 17:45:59 -0300

    se agregan validaciones de mail, documento, y se agrega el form de create usuario, sin llamadas a bll o dal

--- UI/UsuYPermisForms/GestionarUsuarioForm.Designer.cs
+++ UI/UsuYPermisForms/GestionarUsuarioForm.Designer.cs
@@ -157,7 +157,7 @@ namespace UI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 290);
+            this.ClientSize = new System.Drawing.Size(350, 290);
             this.Controls.Add(this.btnCrear);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblNombre);
